4bf1398fb6c0810dd8641bb2bd668c47a3a81a54
irisadmin <irisadmin@dnum>
2025-02-17 16:25:35 +0100

    feat: angular/8-pure-pipe

--- apps/angular/8-pure-pipe/src/app/app.component.ts
+++ apps/angular/8-pure-pipe/src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeavyComputationPipe } from './heavy-computation.pipe';
 
 @Component({
-  imports: [NgFor],
-  selector: 'app-root',
-  template: `
-    <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
-    </div>
-  `,
+    imports: [HeavyComputationPipe],
+    selector: 'app-root',
+    template: `
+        @for (person of persons; track person) {
+            <div>
+                {{ person | heavyComputation: persons.indexOf(person) }}
+            </div>
+        }
+    `,
 })
 export class AppComponent {
-  persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
+    persons = ['toto', 'jack'];
 }
