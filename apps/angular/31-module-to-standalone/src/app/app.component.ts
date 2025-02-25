8250ccb380a7085beb6f44540845d8c8dbbf2678
irisadmin <irisadmin@dnum>
2025-02-25 16:36:45 +0100

    feat: 31-module-to-standalone

--- apps/angular/31-module-to-standalone/src/app/app.component.ts
+++ apps/angular/31-module-to-standalone/src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div class="flex gap-2">
-      <button
-        routerLink="home"
-        class="rounded-md border border-blue-400 px-4 py-2">
-        Home
-      </button>
-      <button
-        routerLink="admin"
-        class="rounded-md border border-blue-400 px-4 py-2">
-        Admin
-      </button>
-      <button
-        routerLink="user"
-        class="rounded-md border border-blue-400 px-4 py-2">
-        User
-      </button>
-    </div>
-    <router-outlet></router-outlet>
-  `,
-  host: {
-    class: 'flex flex-col p-4 gap-3',
-  },
-  standalone: false,
+    selector: 'app-root',
+    template: `
+        <div class="flex gap-2">
+            <button
+                routerLink="home"
+                class="rounded-md border border-blue-400 px-4 py-2">
+                Home
+            </button>
+            <button
+                routerLink="admin"
+                class="rounded-md border border-blue-400 px-4 py-2">
+                Admin
+            </button>
+            <button
+                routerLink="user"
+                class="rounded-md border border-blue-400 px-4 py-2">
+                User
+            </button>
+        </div>
+        <router-outlet></router-outlet>
+    `,
+    host: {
+        class: 'flex flex-col p-4 gap-3',
+    },
+    standalone: true,
+    imports: [RouterLink, RouterOutlet, RouterModule],
 })
 export class AppComponent {}
--- apps/angular/31-module-to-standalone/src/main.ts
+++ apps/angular/31-module-to-standalone/src/main.ts
@@ -1,6 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
+
+const routes: Routes = [{ path: '**', redirectTo: '' }];
+
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(routes)],
+}).catch((err) => console.error(err));
