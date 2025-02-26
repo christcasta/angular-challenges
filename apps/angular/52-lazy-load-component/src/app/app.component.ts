5b194f38eaad8e0e47b8d09b804abf018d00dc20
irisadmin <irisadmin@dnum>
2025-02-26 11:18:58 +0100

    feat: 52-lazy-load-component

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,23 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
-        <app-top />
-      } @else {
-        <app-placeholder />
-        <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
-          Load Top
-        </button>
-      }
-    </div>
-  `,
-  standalone: false,
+    selector: 'app-root',
+    template: `
+        <div class="h-screen bg-gray-500">
+            @defer (when isClicked) {
+                <app-top />
+            } @placeholder {
+                <app-placeholder />
+                <button
+                    class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+                    (click)="onClick()">
+                    Load Top
+                </button>
+            }
+        </div>
+    `,
+    imports: [PlaceholderComponent, TopComponent],
 })
 export class AppComponent {
-  topLoaded = signal(false);
+    isClicked = false;
+    onClick() {
+        this.isClicked = !this.isClicked;
+        console.log(AppComponent.name, this.onClick.name, this.isClicked);
+    }
 }
--- apps/angular/52-lazy-load-component/src/app/top.component.ts
+++ apps/angular/52-lazy-load-component/src/app/top.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-top',
-  template: `
-    I am a very heavy, expensive component that should be lazy loaded.
-  `,
-  styles: `
-    :host {
-      display: grid;
-      padding: 20px;
-      background-color: #f0f0f0;
-      height: 50%;
-    }
-  `,
-  standalone: false,
+    selector: 'app-top',
+    template: `
+        I am a very heavy, expensive component that should be lazy loaded.
+    `,
+    styles: `
+        :host {
+            display: grid;
+            padding: 20px;
+            background-color: #f0f0f0;
+            height: 50%;
+        }
+    `,
 })
-export class TopComponent {}
+export class TopComponent implements OnInit {
+    ngOnInit(): void {
+        console.log(TopComponent.name, this.ngOnInit.name);
+    }
+}
