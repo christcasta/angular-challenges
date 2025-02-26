5694ba2ede7820ac49087723f845c7a73fbbcc7c
irisadmin <irisadmin@dnum>
2025-02-26 11:18:58 +0100

    feat: 52-lazy-load-component

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,23 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
-        <app-top />
-      } @else {
-        <app-placeholder />
-        <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
-          Load Top
-        </button>
-      }
-    </div>
-  `,
-  standalone: false,
+    selector: 'app-root',
+    template: `
+        <div class="h-screen bg-gray-500">
+            @defer (on interaction(click)) {
+                <app-top />
+            } @placeholder {
+                <app-placeholder />
+                <button
+                    class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+                    #click>
+                    Load Top
+                </button>
+            }
+        </div>
+    `,
+    imports: [PlaceholderComponent, TopComponent],
 })
-export class AppComponent {
-  topLoaded = signal(false);
-}
+export class AppComponent {}
