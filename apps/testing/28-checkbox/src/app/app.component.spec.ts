6538e3f338a1ba58f105f1c9aa73cb998aabe829
irisadmin <irisadmin@dnum>
2025-02-27 15:22:34 +0100

    feat: 28-checkbox

--- apps/testing/28-checkbox/src/app/app.component.spec.ts
+++ apps/testing/28-checkbox/src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
-import { render } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  describe('When checking the checkbox', () => {
-    it('Then button is enabled', async () => {
-      await render(AppComponent);
+    describe('When checking the checkbox', () => {
+        it('Then button is enabled', async () => {
+            await render(AppComponent);
+            const checkbox = screen.getByRole('checkbox');
+            const button = screen.getByRole('button');
+            expect(checkbox).not.toBeChecked();
+            expect(button).not.toBeEnabled();
+            fireEvent.click(checkbox);
+            expect(checkbox).toBeChecked();
+            expect(button).toBeEnabled();
+        });
     });
-  });
 });
