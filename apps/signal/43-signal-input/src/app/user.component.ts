3624ddb855aa66c4aa41236112a2fe6ef730c4b0
irisadmin <irisadmin@dnum>
2025-03-04 10:29:21 +0100

    feat: 43-signal-input

--- apps/signal/43-signal-input/src/app/user.component.ts
+++ apps/signal/43-signal-input/src/app/user.component.ts
@@ -1,40 +1,37 @@
 import { TitleCasePipe } from '@angular/common';
 import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
+    ChangeDetectionStrategy,
+    Component,
+    computed,
+    input,
+    numberAttribute,
 } from '@angular/core';
 
 type Category = 'Youth' | 'Junior' | 'Open' | 'Senior';
-const ageToCategory = (age: number): Category => {
-  if (age < 10) return 'Youth';
-  else if (age < 18) return 'Junior';
-  else if (age < 35) return 'Open';
-  return 'Senior';
-};
 
 @Component({
-  selector: 'app-user',
-  imports: [TitleCasePipe],
-  template: `
-    {{ fullName | titlecase }} plays tennis in the {{ category }} category!!
-  `,
-  host: {
-    class: 'text-xl text-green-800',
-  },
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-user',
+    imports: [TitleCasePipe],
+    template: `
+        {{ fullName() | titlecase }} plays tennis in the
+        {{ category() }} category!!
+    `,
+    host: {
+        class: 'text-xl text-green-800',
+    },
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserComponent implements OnChanges {
-  @Input({ required: true }) name!: string;
-  @Input() lastName?: string;
-  @Input() age?: string;
-
-  fullName = '';
-  category: Category = 'Junior';
-
-  ngOnChanges(): void {
-    this.fullName = `${this.name} ${this.lastName ?? ''}`;
-    this.category = ageToCategory(Number(this.age) ?? 0);
-  }
+export class UserComponent {
+    name = input.required<string>();
+    lastName = input<string>();
+    age = input<number, unknown>(0, { transform: numberAttribute });
+    fullName = computed(() => {
+        return `${this.name()} ${this.lastName() ?? ''}`;
+    });
+    category = computed(() => {
+        if (this.age() < 10) return 'Youth';
+        else if (this.age() < 18) return 'Junior';
+        else if (this.age() < 35) return 'Open';
+        return 'Senior';
+    });
 }
