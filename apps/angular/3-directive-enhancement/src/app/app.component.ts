1948122d6327509b71d790b226d3c761bd936818
irisadmin <irisadmin@dnum>
2025-03-10 13:25:10 +0100

    feat: 3-directive-enhancement

--- apps/angular/3-directive-enhancement/src/app/app.component.ts
+++ apps/angular/3-directive-enhancement/src/app/app.component.ts
@@ -2,23 +2,24 @@ import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 interface Person {
-  name: string;
+    name: string;
 }
 
 @Component({
-  imports: [NgFor, NgIf],
-  selector: 'app-root',
-  template: `
-    <ng-container *ngIf="persons.length > 0; else emptyList">
-      <div *ngFor="let person of persons">
-        {{ person.name }}
-      </div>
-    </ng-container>
-    <ng-template #emptyList>The list is empty !!</ng-template>
-  `,
-  styles: [],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [NgFor, NgIf],
+    selector: 'app-root',
+    template: `
+        @for (person of persons; track person.name) {
+            <div>
+                {{ person.name }}
+            </div>
+        } @empty {
+            <div>The list is empty !!</div>
+        }
+    `,
+    styles: [],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  persons: Person[] = [];
+    persons: Person[] = [];
 }
